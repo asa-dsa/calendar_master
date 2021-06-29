05445fe80af050f3be0b4be8849e9a023e0c40b7
salvatore <salvatore.sur@gmail.com>
2021-06-29 14:21:05 +0200

    Added dynamic calendar type buttons Add dynamic events view

--- src/Graphical_Components/CalendarPopup.js
+++ src/Graphical_Components/CalendarPopup.js
@@ -10,7 +10,6 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import 'font-awesome/css/font-awesome.min.css'
 
 import 'react-big-calendar/lib/sass/styles.scss'
-import events from "../events";
 
 import localizer from "react-big-calendar/lib/localizers/globalize";
 import globalize from "globalize";
@@ -25,12 +24,41 @@ class CalendarPopup extends Component {
 
     constructor(...args) {
         super(...args)
-        this.state = {events, culture: 'it'}
+        this.get_event_uri = 'http://0.0.0.0:12345/'
+        this.state = {events:[], culture: 'it'}
+        this.handler = this.handler.bind(this)
     }
+
+    componentDidMount(){
+        this.connectToServer();
+    }
+
+    connectToServer() {
+        this.setState({events:[]})
+        console.log("Connecting to server at this uri:" + this.get_event_uri)
+        axios.get(this.get_event_uri)
+            .then(response => {
+                //.log(response.data)
+                this.setState({events: response.data});
+            })
+            .catch(error =>{
+                console.log(error);
+                this.setState({error: 'Error'});
+            })
+    }
+
+    handler = (val) => {
+        this.get_event_uri = this.get_event_uri + "type=" +val
+        this.connectToServer();
+        console.log("Hey, I'm the handler")
+        console.log(this.get_event_uri)
+    }
+
+
 
     handleSelect = ({start, end}) => {
        // this.setState('url')
-        const title = window.prompt('New Event name')
+        /*const title = window.prompt('New Event name')
         const calendar = window.prompt('Event from this Calendar:')
         if (title)
             this.setState({
@@ -43,7 +71,7 @@ class CalendarPopup extends Component {
                         calendar,
                     },
                 ],
-            })
+            })*/
     }
 
     backgroundColor = (calendarName) => {
@@ -52,10 +80,17 @@ class CalendarPopup extends Component {
     }
 
     render(){
+
+        const {events} = this.state;
+        if(this.state.events.length){
+            events.map((event, index) =>{
+                console.log(event)
+            })
+        }
         return(
             <React.Fragment>
                <div>
-                <Header/>
+                <Header handler={this.handler}/>
                </div>
                    <Calendar
                     popup
@@ -86,7 +121,8 @@ class Header extends Component{
         this.state = {
             posts : [],
         }
-        this.uri = 'https://jsonplaceholder.typicode.com/posts';
+        this.get_calendar_names_uri = 'http://0.0.0.0:12345/cal';
+
     }
 
     componentDidMount(){
@@ -94,7 +130,7 @@ class Header extends Component{
     }
 
     callServer() {
-        axios.get(this.uri)
+        axios.get(this.get_calendar_names_uri)
             .then(response => {
                 //console.log(response.data)
                 this.setState({posts: response.data});
@@ -107,28 +143,33 @@ class Header extends Component{
 
 
     render(){
+        let cal = [], toSend= [];
         const {posts} = this.state;
         if(this.state.posts.length){
             posts.map((post, index) =>{
-                console.log(post.id)
+                cal[index] = post.Type
             })
         }
-        const cal = ['lavoro', 'home'];
         const calNumber = [...Array(cal.length)];
+        for (var i=0; i< calNumber.length; i++)
+            calNumber[i]=false
 
         const handleChange = event  =>{
+
             if(!(calNumber)[event.target.id]) {
-                this.uri= this.uri+(event.target.value.toString())+"-"
-                console.log(this.uri)
-                console.log(event.target.value)
+                this.get_calendar_names_uri= this.get_calendar_names_uri+(event.target.value.toString())+"-"
+               // console.log(this.uri)
+               // console.log(event.target.value)
                 calNumber[event.target.id] = true;
+
                 //this.callServer();
             }
             else {
                 calNumber[event.target.id] = false;
-                this.uri= this.uri.replace((event.target.value+"-"), "")
-                console.log(this.uri)
-                console.log("Non " + event.target.value);
+                this.get_calendar_names_uri= this.get_calendar_names_uri.replace((event.target.value+"-"), "")
+
+                //console.log(this.uri)
+                //console.log("Non " + event.target.value);
             }
         }
     //const {posts} = this.state;
@@ -136,8 +177,14 @@ class Header extends Component{
           //  console.log(posts)
 
 
+
         const handleClick = () =>{
-            console.log(this.uri)
+            var toSend = []
+            let j = 0;
+            for(var i=0; i<calNumber.length; i++)
+                if(calNumber[i])
+                    toSend[j++] = cal[i]
+            this.props.handler(toSend)
         }
 
 
