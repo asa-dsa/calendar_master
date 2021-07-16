0ef96d851e500651d9163becf79c25d576b490c1
salvatore <salvatore.sur@gmail.com>
2021-07-16 12:12:11 +0200

    - Added mother class to implement login

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import CalendarPopup from "./Graphical_Components/CalendarPopup";
+
+
+function App() {
+    return (
+        <div className="App">
+            <header className="App-header">
+                <CalendarPopup/>
+            </header>
+        </div>
+    );
+}
+
+export default App;
